045e8494bdabab21e48525c0b3609307dbb32591
Mario Y. Shinzato <marioyasuo2@gmail.com>
2020-04-07 21:00:18 -0300

    Ajustes no login.

---
+++ Daycoval.Selenium.Framework/Parametros/DayconnectUrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Daycoval.Selenium.Framework.Parametros
+{
+    public static class DayconnectUrl
+    {
+        public static string Login { get; } = "https://ecode.daycoval.com.br";
+        public static string Home { get => new StringBuilder().Append(Login).Append("Home/Default").ToString(); }
+        public static string Sair { get => new StringBuilder().Append(Login).Append("Home/Sair").ToString(); }
+        public static string RendaFixa
+        {
+            get => new StringBuilder().Append(Login)
+                .Append("Investimentos/Aplicacao/SolicitaAplicacaoRendaFixa").ToString();
+        }
+        public static string Fundos
+        {
+            get => new StringBuilder().Append(Login)
+                .Append("Investimentos/Aplicacao/SolicitaAplicacaoRendaFixa").ToString();
+        }
+        public static string AvisoGeral { get => new StringBuilder().Append(Login).Append("Aviso/AvisoGeral").ToString(); }
+    }
+}
---
+++ Daycoval.Selenium.Framework/Parametros/Sistema.cs
@@ -0,0 +1,11 @@
+﻿namespace Daycoval.Selenium.Framework.Parametros
+{
+    public static class Sistema
+    {
+        public static string Login = "";
+        public static string Senha = "";
+        public static string NomeAqruivoBotao = "BotaoSenha{0}.jpg";
+        public static string DirTessData = "";
+        public static string DirImagens = "";
+    }
+}
